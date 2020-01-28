0fdef4798a1f30c8b8d266c1f15bae899f6933e8
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-28 19:38:37 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+#my_collect = ("Tim Jones", "Tom Smith", "Jim Campagno") do |name|
+  name.split(" ").first
+end
 
-
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
