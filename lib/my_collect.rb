b5e9287426cff715cc93e4daae416d01af3895da
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-28 19:37:03 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,5 @@
 array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-my_collect = ("Tim Jones", "Tom Smith", "Jim Campagno") do |name|
+#my_collect = ("Tim Jones", "Tom Smith", "Jim Campagno") do |name|
   name.split(" ").first
 end
 
